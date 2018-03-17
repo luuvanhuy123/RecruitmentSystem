6b951fc4b24995c59e8705bb325849ae5341befb
HUY LUU <luuvanhuy123@gmail.com>
2018-03-17 16:12:58 +0700

    a

--- RecruitmentSystem/src/recruitment/system/interfaceinf/InterfaceUser.java
+++ RecruitmentSystem/src/recruitment/system/interfaceinf/InterfaceUser.java
@@ -1,5 +1,10 @@
 package recruitment.system.interfaceinf;
 
+import java.util.ArrayList;
+
+import recruitment.system.entities.JobPost;
+
 public interface InterfaceUser {
-	
+	boolean jobPosts(JobPost jobPost);
+	ArrayList<JobPost> getListRecruitment(int type);
 }
